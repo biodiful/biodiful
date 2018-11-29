9b966a735bd54c519a3e8a3beb4e528a1763ef6a
Thomas Bouttaz <thomas.bouttaz@gmail.com>
2018-11-29 15:35:15 +0100

    Add WYSIWYG editor on survey's text areas

--- src/main/webapp/app/entities/survey/survey.module.ts
+++ src/main/webapp/app/entities/survey/survey.module.ts
@@ -1,5 +1,7 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularEditorModule } from '@kolkov/angular-editor';
 
 import { BiodifulSharedModule } from 'app/shared';
 import {
@@ -15,7 +17,7 @@ import {
 const ENTITY_STATES = [...surveyRoute, ...surveyPopupRoute];
 
 @NgModule({
-    imports: [BiodifulSharedModule, RouterModule.forChild(ENTITY_STATES)],
+    imports: [BiodifulSharedModule, RouterModule.forChild(ENTITY_STATES), HttpClientModule, AngularEditorModule],
     declarations: [SurveyComponent, SurveyDetailComponent, SurveyUpdateComponent, SurveyDeleteDialogComponent, SurveyDeletePopupComponent],
     entryComponents: [SurveyComponent, SurveyUpdateComponent, SurveyDeleteDialogComponent, SurveyDeletePopupComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
