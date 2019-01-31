52abc8fe4ad9ad7200cea434688ba8e2ae777c4b
Thomas Bouttaz <thomas.bouttaz@gmail.com>
2019-01-31 16:39:56 +0100

    remove Encapsulation on footer

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
     selector: 'jhi-footer',
     templateUrl: './footer.component.html',
-    styleUrls: ['footer.css']
+    styleUrls: ['footer.css'],
+    encapsulation: ViewEncapsulation.None
 })
 export class FooterComponent {
     constructor(public router: Router) {}
